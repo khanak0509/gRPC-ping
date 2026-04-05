8b214cf0ac4fb1893fbb49218482ba731a2b1d30
khanak0509 <kkhanak512@gmail.com>
2026-04-06 00:19:42 +0530

    minor fixes

--- bin/grpc_ping.dart
+++ bin/grpc_ping.dart
@@ -1,5 +1,4 @@
 import 'package:args/args.dart';
-
 import 'package:grpc_ping/client.dart';
 import 'package:grpc_ping/printer.dart';
 import 'package:grpc_ping/result.dart';
@@ -7,55 +6,48 @@ import 'package:grpc_ping/server.dart';
 
 Future<void> main(List<String> args) async {
   final parser = ArgParser()
-    ..addOption('host', defaultsTo: 'localhost', help: 'Server host')
-    ..addOption('port', defaultsTo: '50051', help: 'Server port')
-    ..addOption('name', defaultsTo: 'grpc_ping', help: 'Name field in HelloRequest')
-    ..addFlag('tls', help: 'Use TLS', defaultsTo: false)
-    ..addFlag('local',
-        defaultsTo: true,
-        negatable: true,
-        help: 'Spin up an embedded Greeter server')
-    ..addOption('call',
-        allowed: ['unary', 'streaming', 'all'],
-        defaultsTo: 'all',
-        help: 'Which RPC(s) to run');
+    ..addOption('host', defaultsTo: 'localhost')
+    ..addOption('port', defaultsTo: '50051')
+    ..addOption('name', defaultsTo: 'grpc_ping')
+    ..addFlag('tls', defaultsTo: false)
+    ..addFlag('local', defaultsTo: true, negatable: true)
+    ..addOption('call', allowed: ['unary', 'streaming', 'all'], defaultsTo: 'all');
 
-  final ns = parser.parse(args);
-  final host = ns['host'] as String;
-  final port = int.parse(ns['port'] as String);
-  final name = ns['name'] as String;
-  final tls = ns['tls'] as bool;
-  final local = ns['local'] as bool;
-  final call = ns['call'] as String;
+  final opts = parser.parse(args);
+  final host = opts['host'] as String;
+  final port = int.parse(opts['port'] as String);
+  final name = opts['name'] as String;
+  final tls = opts['tls'] as bool;
+  final local = opts['local'] as bool;
+  final call = opts['call'] as String;
 
-  LocalServer? embedded;
-  var connectHost = host;
-  var connectPort = port;
+  LocalServer? server;
+  late final PingClient client;
+  var actualHost = host;
+  var actualPort = port;
 
   if (local) {
-    embedded = LocalServer();
-    await embedded.start();
-    connectHost = 'localhost';
-    connectPort = embedded.port;
+    server = LocalServer();
+    await server.start();
+    actualHost = 'localhost';
+    actualPort = server.port;
+    client = PingClient(actualHost, actualPort, tls: tls);
+  } else {
+    client = PingClient(host, port, tls: tls);
   }
 
-  final client = PingClient(connectHost, connectPort, tls: tls);
   final results = <PingResult>[];
-
-  try {
-    if (call == 'unary' || call == 'all') {
-      results.add(await client.unary(name: name));
-    }
-    if (call == 'streaming' || call == 'all') {
-      results.add(await client.streaming(name: name));
-    }
-  } finally {
-    await client.close();
-    if (embedded != null) {
-      await embedded.stop();
-    }
+  if (call == 'unary' || call == 'all') {
+    results.add(await client.unary(name: name));
+  }
+  if (call == 'streaming' || call == 'all') {
+    results.add(await client.streaming(name: name));
   }
 
-  final target = '$connectHost:$connectPort';
-  Printer().printTable(results, target: target);
+  Printer().printTable(results, target: '$actualHost:$actualPort');
+
+  await client.close();
+  if (server != null) {
+    await server.stop();
+  }
 }
